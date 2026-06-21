5fd6bc6c432a38936f812a1d7699d9fdb6077465
Anthony Marques <anthonymarques011002@gmail.com>
2026-06-21 00:46:53 -0300

    Add CarroTeste to test Carro class

---
+++ CarroTeste.java
@@ -0,0 +1,9 @@
+public class CarroTeste {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro("BYD", "sedan", 2026);
+        carro1.acelerar();
+        carro1.acelerar();
+        carro1.acelerar();
+        carro1.frear();
+    }
+}

94ab0dceb0d68b69d0b836cb9c7ae4c0452a38c2
Anthony Marques <anthonymarques011002@gmail.com>
2026-06-21 00:51:18 -0300

    Add PessoaTeste class to demonstrate Pessoa functionality

---
+++ PessoaTeste.java
@@ -0,0 +1,9 @@
+public class PessoaTeste {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("Tony", 23, 1.72);
+        pessoa1.apresentar();
+
+        Pessoa pessoa2 = new Pessoa("Lais", 16, 1.64);
+        pessoa2.apresentar();
+    }
+}
